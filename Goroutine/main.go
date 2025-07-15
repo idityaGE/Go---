cfd7dfa9f7880654e6fd2604256fe23af789edaf
idityaGE <am44910606@gmail.com>
2025-07-15 20:11:57 +0530

    Refactor prime number calculation and worker implementation for improved clarity and performance

--- Goroutine/main.go
+++ Goroutine/main.go
@@ -2,33 +2,65 @@ package main
 
 import (
 	"fmt"
-	// "sync"
-	"time"
+	"sync"
 )
 
-func main() {
-	jobs := make(chan int, 1000)
-	results := make(chan int, 1000)
-
-	for i := range 5 {
-		go worker(i+1, jobs, results)
+func isPrime(num int) bool {
+	if num < 2 {
+		return false
 	}
-
-	for i := range 1000 {
-		jobs <- i + 1
+	for i := 2; i*i < num; i++ {
+		if num%i == 0 {
+			return false
+		}
 	}
-	close(jobs)
+	return true
+}
 
-	for i := 0; i < 1000; i++ {
-		<-results
+func worker(wg *sync.WaitGroup, jobs <-chan int, results chan<- int) {
+	defer wg.Done()
+	fmt.Println("Waiting")
+	for num := range jobs {
+		if isPrime(num) {
+			results <- num
+		}
 	}
 }
 
-func worker(id int, jobs <-chan int, results chan<- int) {
-	for job := range jobs {
-		fmt.Printf("Worker %d: started job %d\n", id, job)
-		time.Sleep(time.Second)
-		fmt.Printf("Worker %d: finished job %d\n", id, job)
-		results <- job * 2
+func collectResult(results <-chan int) int {
+	sum := 0
+	for prime := range results {
+		sum += prime
 	}
+	return sum
+}
+
+func main() {
+	var wg sync.WaitGroup
+	const numofWorker = 8
+	const maxNum = 10000
+
+	jobs := make(chan int, 100)
+	results := make(chan int, 100)
+
+	for range numofWorker {
+		wg.Add(1)
+		go worker(&wg, jobs, results)
+	}
+
+	go func() {
+		for num := range maxNum {
+			jobs <- num
+		}
+		close(jobs)
+	}()
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	totalSum := collectResult(results)
+
+	fmt.Println(totalSum)
 }
